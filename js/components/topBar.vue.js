afaf4a095720bc804e0053c822139968380dc56f
Julio Contreras <julio.contreras@s2grupo.es>
2021-08-17 22:40:52 +0200

    fixed topbar padding

--- js/components/topBar.vue.js
+++ js/components/topBar.vue.js
@@ -1,9 +1,11 @@
 Vue.component('top-bar', {
   template: `
-  <div class="flex w-full justify-end px-6">
-    <div class="pr-2 underline cursor-pointer" v-on:click="day"> Day </div>
-    <div class="pr-2 underline cursor-pointer" v-on:click="night"> Night </div>
-    <a href="https://www.linkedin.com/in/julio-contreras-6579b623/?originalSubdomain=es" target="_blank"> Linkeind </a>
+  <div class="flex">
+    <div class="flex justify-end w-full px-6">
+      <div class="pr-2 underline cursor-pointer" v-on:click="day"> Day </div>
+      <div class="pr-2 underline cursor-pointer" v-on:click="night"> Night </div>
+      <a href="https://www.linkedin.com/in/julio-contreras-6579b623/?originalSubdomain=es" target="_blank"> Linkeind </a>
+    </div>
   </div>`,
   methods: {
     day () {
