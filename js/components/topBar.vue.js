7684dd288d0607d75a800950d89a5b64cc912816
Julio Contreras <julio.contreras@s2grupo.es>
2021-08-17 22:44:46 +0200

    test padding

--- js/components/topBar.vue.js
+++ js/components/topBar.vue.js
@@ -1,7 +1,7 @@
 Vue.component('top-bar', {
   template: `
   <div class="flex">
-    <div class="flex justify-end w-full px-6">
+    <div class="flex justify-end w-full pl-6 pr-9">
       <div class="pr-2 underline cursor-pointer" v-on:click="day"> Day </div>
       <div class="pr-2 underline cursor-pointer" v-on:click="night"> Night </div>
       <a href="https://www.linkedin.com/in/julio-contreras-6579b623/?originalSubdomain=es" target="_blank"> Linkeind </a>
