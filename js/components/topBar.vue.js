6fc5e5eb0cc8b38839c821f0ff1f95f1e9baa4ef
Julio Contreras <julio.contreras@s2grupo.es>
2021-08-17 22:57:35 +0200

    fix links topbar

--- js/components/topBar.vue.js
+++ js/components/topBar.vue.js
@@ -2,19 +2,30 @@ Vue.component('top-bar', {
   template: `
   <div class="flex">
     <div class="flex justify-end w-full pl-6 pr-9">
-      <div class="pr-2 underline cursor-pointer" v-on:click="day"> Day </div>
-      <div class="pr-2 underline cursor-pointer" v-on:click="night"> Night </div>
-      <a href="https://www.linkedin.com/in/julio-contreras-6579b623/?originalSubdomain=es" target="_blank"> Linkeind </a>
+      <a href="javascript:void()" aria-label="Day theme" v-if="!changeForDay" class="pr-2 no-underline cursor-pointer" v-on:click="day"> ☀️ </a>
+      <a href="javascript:void()" aria-label="Night theme" v-if="changeForDay" class="pr-2 no-underline cursor-pointer" v-on:click="night"> 🌑 </a>
+      <a
+        href="https://www.linkedin.com/in/julio-contreras-6579b623/?originalSubdomain=es"
+        target="_blank"
+        class="pt-1"
+      > Linkedin </a>
     </div>
   </div>`,
+  data () {
+    return {
+      changeForDay: false
+    }
+  },
   methods: {
     day () {
       document.getElementById("body").classList.add('light');
       document.getElementById("body").classList.remove('dark');
+      this.changeForDay = true
     },
     night () {
       document.getElementById("body").classList.add('dark');
       document.getElementById("body").classList.remove('light');
+      this.changeForDay = false
     }
   }
 })
